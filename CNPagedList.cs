b83a83e65dd470f8977ac5a65ab7ab2e7f364396
jonnale <jonnalewajek@gmail.com>
2014-08-14 11:28:39 -0400

    Update CNPagedList.cs

--- CNPagedList.cs
+++ CNPagedList.cs
@@ -1,78 +1,87 @@
-public class CNPagedList<T>
+using System.Linq;
+
+namespace CN.Core
 {
     /// <summary>
-    /// Constructor
+    /// A class that handles pagination of an IQueryable
     /// </summary>
-    /// <param name="list">The full list of items you would like to paginate</param>
-    /// <param name="page">(optional) The current page number</param>
-    /// <param name="pageSize">(optional) The size of the page</param>
-    public CNPagedList(IQueryable<T> list, int?page=null, int?pageSize=null)
+    /// <typeparam name="T"></typeparam>
+    public class CNPagedList<T>
     {
-        _list = list;
-        _page = page;
-        _pageSize = pageSize;
-    }
-
-    private IQueryable<T> _list;
-    
-    /// <summary>
-    /// The paginated result
-    /// </summary>
-    public IQueryable<T> items
-    {
-        get
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="list">The full list of items you would like to paginate</param>
+        /// <param name="page">(optional) The current page number</param>
+        /// <param name="pageSize">(optional) The size of the page</param>
+        public CNPagedList(IQueryable<T> list, int?page=null, int?pageSize=null)
         {
-            if (_list == null) return null;
-            return _list.Skip((page - 1) * pageSize).Take(pageSize);
+            _list = list;
+            _page = page;
+            _pageSize = pageSize;
         }
-    }
 
-    private int? _page;
-    /// <summary>
-    ///  The current page.
-    /// </summary>
-    public int page
-    {
-        get
+        private IQueryable<T> _list;
+        
+        /// <summary>
+        /// The paginated result
+        /// </summary>
+        public IQueryable<T> items
         {
-            if (!_page.HasValue)
+            get
             {
-                return 1;
-            }
-            else
-            {
-                return _page.Value;
+                if (_list == null) return null;
+                return _list.Skip((page - 1) * pageSize).Take(pageSize);
             }
         }
-    }
 
-    private int? _pageSize;
-    /// <summary>
-    /// The size of the page.
-    /// </summary>
-    public int pageSize
-    {
-        get
+        private int? _page;
+        /// <summary>
+        ///  The current page.
+        /// </summary>
+        public int page
         {
-            if (!_pageSize.HasValue)
+            get
+            {
+                if (!_page.HasValue)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return _page.Value;
+                }
+            }
+        }
+
+        private int? _pageSize;
+        /// <summary>
+        /// The size of the page.
+        /// </summary>
+        public int pageSize
+        {
+            get
+            {
+                if (!_pageSize.HasValue)
+                {
+                    return _list == null ? 0 : _list.Count();
+                }
+                else
+                {
+                    return _pageSize.Value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The total number of items in the original list of items.
+        /// </summary>
+        public int totalItemCount
+        {
+            get
             {
                 return _list == null ? 0 : _list.Count();
             }
-            else
-            {
-                return _pageSize.Value;
-            }
-        }
-    }
-
-    /// <summary>
-    /// The total number of items in the original list of items.
-    /// </summary>
-    public int totalItemCount
-    {
-        get
-        {
-            return _list == null ? 0 : _list.Count();
         }
     }
 }
